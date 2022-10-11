3ce151854a90bc2163feef23c176f98ccf697390
Avinaw Kumar <0811kumaravinaw@gmail.com>
2022-10-11 21:36:08 +0530

    Mathematical Operator

---
+++ WEB23-MATHEMATICAL-OPERATOR/P1.js
@@ -0,0 +1,7 @@
+let a = 2;
+let b = 3;
+
+console.log('sum is ', a+b);
+console.log('sub is', a-b);
+console.log('melt is' ,a*b);
+console.log('div is ', a/b)
---
+++ WEB23-MATHEMATICAL-OPERATOR/p2.js
@@ -0,0 +1,8 @@
+// calculate the square root of x
+
+let x = 16 
+console.log(x** 0.5);
+
+let b= 12
+let y= 2;
+console.log((b+y)**);
